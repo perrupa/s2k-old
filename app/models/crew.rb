05b8ac3b7b30283b3ef2a481910d1ccf57ec42a8
unknown <cmarlow@.(none)>
2011-04-05 14:59:11 -0400

    Added videos to the Crew model

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,3 +1,10 @@
 class Crew < ActiveRecord::Base
 	has_and_belongs_to_many :bboys
+	has_many :video_participants, 
+		:class_name => "VideoParticipant",
+		:foreign_key => "crew_id"
+	has_many :videos, 
+		:through => :video_participants,
+		:readonly => true
+
 end
